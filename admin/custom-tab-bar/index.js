5bf604954da99093994fb3d6d8af5efcf354f634
Shoulaxiao <2660209659@qq.com>
2021-03-04 16:57:43 +0800

    前端导航栏tab显示

--- admin/custom-tab-bar/index.js
+++ admin/custom-tab-bar/index.js
@@ -2,29 +2,33 @@ Component({
     data: {
         selected: 0,
         color: "#7A7E83",
+        backgroundColor: "#ffffff",
         selectedColor: "#3cc51f",
         list: [
             {
                 "pagePath": "pages/index/index",
-                "iconPath": "assets/images/icon_component.png",
-                "selectedIconPath": "assets/images/icon_component_HL.png",
-                "text": "组件"
+                "iconPath": "../image/icon_component.png",
+                "selectedIconPath": "../image/icon_component_HL.png",
+                "text": "分类"
             },
             {
                 "pagePath": "pages/user/user",
-                "iconPath": "assets/images/icon_API.png",
-                "selectedIconPath": "assets/images/icon_API_HL.png",
-                "text": "接口"
+                "iconPath": "../image/icon_API.png",
+                "selectedIconPath": "../image/icon_API_HL.png",
+                "text": "我的"
             }
         ]
     },
     attached() {
     },
     methods: {
-        switchTab(e) {
-            const data = e.currentTarget.dataset
+        switchTab(event) {
+            console.log("switchTab=>",event)
+            const data = event.currentTarget.dataset
             const url = data.path
-            wx.switchTab({url})
+            wx.switchTab({
+                url
+            })
             this.setData({
                 selected: data.index
             })
